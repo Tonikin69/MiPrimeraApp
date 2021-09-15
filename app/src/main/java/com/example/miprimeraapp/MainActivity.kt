7c7a6fa61d37af4119040e1c0283b8fc17c56169
Antonio <antoniotrotuman@gmail.com>
2021-09-15 11:10:50 +0200

    añadido los idiomas

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.miprimeraapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, getString(R.string.hello), Toast.LENGTH_LONG).show()
         Log.v("MainActivity", "Se ha creado correctamente")
 //hola
         //adios
+
 
     }
 }
