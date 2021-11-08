c580a963f0a6f941e31d9227e5845b46456164df
Antonio <antoniotrotuman@gmail.com>
2021-11-08 12:05:26 +0100

    e añadido mas cosas

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.HorizontalScrollView
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -12,23 +13,29 @@ import androidx.core.view.isVisible
 class MainActivity : AppCompatActivity() {
     lateinit var imageViewPueblo : ImageView
     lateinit var textPueblo : TextView
+    lateinit var scrollPueblo : HorizontalScrollView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.amazon)
         imageViewPueblo =findViewById<ImageView>(R.id.clika)
         textPueblo=findViewById<TextView>(R.id.clika2)
+        scrollPueblo=findViewById<HorizontalScrollView>(R.id.clika3)
         imageViewPueblo.setOnClickListener {
             saludaAmazon()
         }
+        scrollPueblo.visibility=View.GONE
         textPueblo.setOnClickListener {
-            aparece()
+            if (scrollPueblo.visibility==View.VISIBLE){
+                scrollPueblo.visibility= View.GONE
+            }else {
+                scrollPueblo.visibility= View.VISIBLE
+            }
+
         }
     }
     fun saludaAmazon(){
         Toast.makeText(this,"Esta imagen mide ${imageViewPueblo.height}",Toast.LENGTH_LONG).show()
 
-    }
-    fun aparece(){
-        imageViewPueblo.isVisible
     }
 }
