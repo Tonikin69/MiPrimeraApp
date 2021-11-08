b5b86536da018fa287a9883dcf90e5ccd8630265
Antonio <antoniotrotuman@gmail.com>
2021-11-08 11:11:04 +0100

    e añadido mas cosas

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -5,20 +5,30 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageViewPueblo : ImageView
+    lateinit var textPueblo : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.amazon)
         imageViewPueblo =findViewById<ImageView>(R.id.clika)
+        textPueblo=findViewById<TextView>(R.id.clika2)
         imageViewPueblo.setOnClickListener {
             saludaAmazon()
+        }
+        textPueblo.setOnClickListener {
+            aparece()
         }
     }
     fun saludaAmazon(){
         Toast.makeText(this,"Esta imagen mide ${imageViewPueblo.height}",Toast.LENGTH_LONG).show()
 
+    }
+    fun aparece(){
+        imageViewPueblo.isVisible
     }
 }
